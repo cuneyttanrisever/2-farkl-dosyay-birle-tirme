6fa699dd00dc31a0eb785d9cab9a546871f13c11
Cüneyt Tanrısever <55285425+cuneyttanrisever@users.noreply.github.com>
2022-07-23 21:07:51 +0300

    Update birlestir.py

--- birlestir.py
+++ birlestir.py
@@ -1,7 +1,13 @@
-oku=open("a.txt","r", encoding="utf-8").readlines()
-oku1=open("b.txt","r", encoding="utf-8").readlines()
-yaz=open("birles.txt","a",encoding="utf-8")
-for i in range(len(oku)):
-    birlestir=oku[i].replace("\n","")+" = "+oku1[i]
+oku=open("a.txt","r", encoding="utf-8")
+dex=oku.readlines()
+oku1=open("b.txt","r", encoding="utf-8")
+dex2=oku1.readlines()
+yaz=open("birles.txt","w",encoding="utf-8")
+for i in range(len(dex)):
+    birlestir=dex[i].replace("\n","")+" = "+dex2[i]
     print (birlestir)
     yaz.write(str(birlestir))
+
+oku.close()
+oku1.close()
+yaz.close()
